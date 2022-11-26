e4f46b9a23223d0b22af66790d71c7c291ca78a5
PRIYANKA CHAUDHARI <117078620+1PRIYANKA1@users.noreply.github.com>
2022-11-26 19:44:36 +0530

    Add files via upload

---
+++ funcEven.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int isEven(int);
+int main()
+{
+	int i,x;
+	cout<<"enter a no.";
+	cin>>i;
+	isEven(i);
+	x=isEven(i);
+	if(x==1)
+	{
+		cout<<"even";
+	}
+	else{
+		cout<<"odd";
+	}
+	              
+}
+int isEven(int i)
+{
+	if(i%2==0)
+	{
+		return 1;
+	}
+	else
+	{
+	    return 0;
+	}
+}
